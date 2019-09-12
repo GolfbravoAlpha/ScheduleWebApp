a9f6aedfe1ad24548ee95e90ffa6d391adc0bfdd
Mahi Rahman <mahi_rahman@outlook.com>
2019-09-12 20:42:02 +0100

    Adding student and staff API. Assign page tested.

---
+++ ScheduleWebApp/Controllers/api/StaffController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScheduleWebApp.Data;
+using ScheduleWebApp.Models;
+
+namespace ScheduleWebApp.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StaffController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/Staff
+        [HttpGet]
+        public IEnumerable<Staff> GetStaff()
+        {
+            //the include allows information from another table to come through 
+             return _context.StaffTbl
+                .ToList();
+        }
+
+        // GET: api/Staff/5
+        [HttpGet("{id}")]
+        public IEnumerable<Staff> GetStaff(int id)      
+        {
+            var staff = from s in _context.StaffTbl
+                        .Where(s => s.id == id)
+                        .ToList()
+                        select s;
+
+            return staff;
+        }
+
+        // POST: api/Staff
+        [HttpPost]
+        public async Task<ActionResult<Staff>> PostStaff
+            (Staff staffObject)
+        {
+            _context.StaffTbl.Add(staffObject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStaff),
+                new { id = staffObject.id }, staffObject);
+        }
+
+        // PUT: api/Staff/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStaff(int id, Staff staffObject)
+        {
+            if (id != staffObject.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(staffObject).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var staffObject = await _context.StaffTbl.FindAsync(id);
+
+            if (staffObject == null)
+            {
+                return NotFound();
+            }
+
+            _context.StaffTbl.Remove(staffObject);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ScheduleWebApp/Controllers/api/StudentController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScheduleWebApp.Data;
+using ScheduleWebApp.Models;
+
+namespace ScheduleWebApp.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        // GET: api/Student
+        [HttpGet]
+        public IEnumerable<Student> GetStudent()
+        {
+            //the include allows information from another table to come through 
+            return _context.StudentTbl
+               .ToList();
+        }
+
+        // GET: api/Student/5
+        [HttpGet("{id}")]
+        public IEnumerable<Student> GetStudent(int id)
+        {
+            var student = from s in _context.StudentTbl
+                        .Where(s => s.id == id)
+                        .ToList()
+                        select s;
+
+            return student;
+        }
+
+        // POST: api/Student
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent
+            (Student studentObject)
+        {
+            _context.StudentTbl.Add(studentObject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStudent),
+                new { id = studentObject.id }, studentObject);
+        }
+
+        // PUT: api/Student/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student studentObject)
+        {
+            if (id != studentObject.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(studentObject).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var studentObject = await _context.StudentTbl.FindAsync(id);
+
+            if (studentObject == null)
+            {
+                return NotFound();
+            }
+
+            _context.StudentTbl.Remove(studentObject);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
